4190dd771c63ede64b6b193c00a7bdb7dbe52d3c
greensnapback0229 <smdmim@gmail.com>
2024-04-03 01:21:14 +0900

    Initial: COMAtching Version2

---
+++ src/main/java/comatching/comatcing/ComatchingApplication.java
@@ -0,0 +1,13 @@
+package comatching.comatcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComatchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comatching/comatcing/ComatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package comatching.comatcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
